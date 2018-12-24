bca57f3cbe8f0a2173ec32c411a1a7c57c0498cd
Jessica Vazquez <jmvazquezcs@gmail.com>
2018-12-24 11:03:18 -0500

    NVM: undo previous commits. We don't need to add readmes and images to build.

--- genie.lua
+++ genie.lua
@@ -9,9 +9,6 @@ local function new_project(name)
         files {
             "tests/"..name.."/*.cpp",
             "tests/"..name.."?*.h",
-            "tests/"..name.."/README.md",
-            "tests/"..name.."/*.png",
-            "tests/"..name.."/*.gif",
             "src/*.cpp",
             "include/**.h",
             "src/deps/glfw/context.c",
