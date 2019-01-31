5605f971623a0404d05498e860e1251c7e7bc6bd
Jessica Vazquez <jmvazquezcs@gmail.com>
2019-01-31 05:29:42 -0500

    #31: code for AG_08_03 - spot light. Project 3 of 5.

---
+++ tests/AG_08_03/cube.fs
@@ -0,0 +1,52 @@
+#version 330 core
+
+in vec3 normal;
+in vec3 fragPos;
+in vec2 textCoords;
+
+out vec4 fragColor;
+
+struct Material {
+    sampler2D diffuse;
+    sampler2D specular;
+    float shininess;
+};
+
+// Spot light
+struct Light {
+    vec3 position;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+
+    float constant;
+    float linear;
+    float quadratic;
+};
+
+uniform Material material; 
+uniform Light light;
+uniform vec3 viewPos;
+
+void main() {
+    vec3 textDiffuse = vec3(texture(material.diffuse, textCoords));
+    vec3 textSpecular = vec3(texture(material.specular, textCoords));
+    vec3 ambient = light.ambient * textDiffuse;
+
+    vec3 norm = normalize(normal);
+    vec3 lightDir = normalize(light.position - fragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * textDiffuse * light.diffuse;
+
+    vec3 viewDir = normalize(viewPos - fragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 specular = spec * textSpecular * light.specular;
+
+    float distance = length(light.position - fragPos);
+    float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
+
+    vec3 phong = (ambient + diffuse + specular) * attenuation;
+    fragColor = vec4(phong, 1.0);
+} 
